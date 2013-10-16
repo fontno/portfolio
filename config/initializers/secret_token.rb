80d142e83b047683dbd7224c3d67a6c1bbc2e1e5
Brian Fontenot <fontenotbrian@gmail.com>
2013-10-15 22:27:11 -0500

    Gemfile and Zeus setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = 'fadd52a1b8c31ad11f2f85ecd4c59300fe8c119e0d6a514c036c2daba1caff7cfc7d4a1a17a14ed9592e4eaf5a590e7660c501e27bdb18ff96ef8453744ba660'
